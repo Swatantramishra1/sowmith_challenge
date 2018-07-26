95b5282ef374ebbd5a5d5de810392aae18eec704
purander <purander@mordorintelligence.com>
2018-07-26 13:11:21 +0530

    Add redux and enable css modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
-import './App.css';
+import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={styles.app}>
         <Button type="primary">Button</Button>
       </div>
     );
