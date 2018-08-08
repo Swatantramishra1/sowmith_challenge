dd9ee8c3b74e5bbda25be24368f0bb80448e259d
Swatantra <swatantra@mordorintelligence.com>
2018-08-08 11:40:13 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import { Button } from 'antd';
 import { connect } from 'react-redux';
 import './App.css';
+import GoogleLogin from 'react-google-login';
 
 class App extends Component {
   render() {
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
     console.log(this.props);
     return (
       <div className="app">
+        <GoogleLogin
+          clientId="575412109893-clfpajd235fpk9tllnilac2n3et81nvh.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
         <Button type="primary">Button</Button>
       </div>
     );
@@ -19,7 +29,7 @@ function mapStateToProps(state) {
 }
 
 const mapDispatchToProps = {
-  
+
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
 import { connect } from 'react-redux';
+
 import './App.css';
 
 class App extends Component {
   render() {
+    
     console.log(this.props);
     return (
       <div className="app">
-        <Button type="primary">Button</Button>
+     
+        {/* <Button type="primary">Button</Button> */}
       </div>
     );
   }
